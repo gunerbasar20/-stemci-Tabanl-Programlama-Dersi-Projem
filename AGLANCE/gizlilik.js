20af1afaef85769d4f55287c98b51a411b1c5df6
gunerbasar20 <gunerbasar20@gmail.com>
2024-08-09 17:00:03 +0300

    Add files via upload

---
+++ AGLANCE/gizlilik.js
@@ -0,0 +1,15 @@
+function showimg1(){
+    document.getElementById("img1").style.zIndex=2;
+    document.getElementById("img2").style.zIndex=1;
+    document.getElementById("img3").style.zIndex=1;
+}
+function showimg2(){
+    document.getElementById("img1").style.zIndex=1;
+    document.getElementById("img2").style.zIndex=2;
+    document.getElementById("img3").style.zIndex=1;
+}
+function showimg3(){
+    document.getElementById("img1").style.zIndex=1;
+    document.getElementById("img2").style.zIndex=1;
+    document.getElementById("img3").style.zIndex=2;
+}
